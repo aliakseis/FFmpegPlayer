60e49f86ddcba3aa47d42c90e6b83e6b2885dde9
aliakseis <aliaksei.sanko@gmail.com>
2018-06-09 19:05:03 +0300

    more YouTube stuff

---
+++ Player/YouTuber.cpp
@@ -0,0 +1,220 @@
+#include "stdafx.h"
+
+#include "YouTuber.h"
+
+//#define YOUTUBE_EXPERIMENT
+
+#ifdef YOUTUBE_EXPERIMENT
+
+#include <Shlobj.h>
+
+#include <boost/python/exec.hpp>
+#include <boost/python/import.hpp>
+#include <boost/python/extract.hpp>
+#include <regex>
+#include <fstream>
+
+#include "unzip.h"
+
+namespace {
+
+const char PYTUBE_URL[] = "https://github.com/nficano/pytube/archive/master.zip";
+
+const char SCRIPT_TEMPLATE[] = R"(import sys
+sys.path.append("%s")
+from pytube import YouTube
+def getYoutubeUrl(url):
+	return YouTube(url).streams.filter(progressive=True).order_by('resolution').desc().first().url)";
+
+
+char* replace_char(char* str, char find, char replace)
+{
+    char *current_pos = strchr(str,find);
+    while (current_pos) {
+        *current_pos = replace;
+        current_pos = strchr(++current_pos, find);
+    }
+    return str;
+}
+
+bool isUrlYoutube(const std::string& url)
+{
+    std::regex txt_regex(R"(^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+)");
+    return std::regex_match(url, txt_regex);
+}
+
+bool DownloadAndExtractZip(const char* zipfile, const TCHAR* root)
+{
+    unzFile uf = unzOpen((voidpf)zipfile);
+    if (!uf)
+    {
+        return false;
+    }
+    unzGoToFirstFile(uf);
+    do {
+        char filename[MAX_PATH];
+        unzGetCurrentFileInfo(uf, 0, filename, sizeof(filename), 0, 0, 0, 0);
+
+        TCHAR path[MAX_PATH];
+        _tcscpy_s(path, root);
+        PathAppend(path, CA2T(filename, CP_UTF8));
+
+        auto pathlen = _tcslen(path);
+        if (pathlen > 0 && (path[pathlen - 1] == _T('/') || path[pathlen - 1] == _T('\\')))
+        {
+            if (_tmkdir(path) != 0)
+                return false;
+        }
+        else
+        {
+            unzOpenCurrentFile(uf);
+
+            std::ofstream f(path, std::ofstream::binary);
+
+            char buf[1024 * 64];
+            int r;
+            do
+            {
+                r = unzReadCurrentFile(uf, buf, sizeof(buf));
+                if (r > 0)
+                {
+                    f.write(buf, r);
+                }
+            } while (r > 0);
+            unzCloseCurrentFile(uf);
+        }
+    } while (unzGoToNextFile(uf) == UNZ_OK);
+
+    unzClose(uf);
+    return true;
+}
+
+class YouTubeDealer 
+{
+public:
+    YouTubeDealer();
+    ~YouTubeDealer();
+
+    bool isValid() const { return m_valid; }
+    std::string getYoutubeUrl(const std::string& url);
+
+private:
+    bool m_valid;
+    boost::python::object m_obj;
+};
+
+
+YouTubeDealer::YouTubeDealer()
+    : m_valid(false)
+{
+    // String buffer for holding the path.
+    TCHAR strPath[MAX_PATH]{};
+
+    // Get the special folder path.
+    SHGetSpecialFolderPath(
+        0,       // Hwnd
+        strPath, // String buffer.
+        CSIDL_LOCAL_APPDATA, // CSLID of folder
+        TRUE); // Create if doesn't exists?}
+
+    CString localAppdataPath = strPath;
+
+    PathAppend(strPath, _T("pytube-master"));
+
+    if (-1 == _taccess(strPath, 0)
+        && !DownloadAndExtractZip(PYTUBE_URL, localAppdataPath))
+    {
+        return;
+    }
+    if (-1 != _taccess(strPath, 0))
+    {
+        using namespace boost::python;
+
+        Py_Initialize();
+        try {
+            // Retrieve the main module.
+            object main = import("__main__");
+
+            // Retrieve the main module's namespace
+            object global(main.attr("__dict__"));
+
+
+            CT2A convert(strPath, CP_UTF8);
+
+            replace_char(convert, '\\', '/');
+
+            char script[4096];
+            sprintf_s(script, SCRIPT_TEMPLATE, static_cast<LPSTR>(convert));
+
+            // Define greet function in Python.
+            object exec_result = exec(script, global, global);
+
+            // Create a reference to it.
+            m_obj = global["getYoutubeUrl"];
+        }
+        catch (const std::exception&)
+        {
+            Py_Finalize();
+            return;
+        }
+        catch (const error_already_set&)
+        {
+            Py_Finalize();
+            return;
+        }
+
+        m_valid = true;
+    }
+
+}
+
+YouTubeDealer::~YouTubeDealer()
+{
+    if (m_valid)
+        Py_Finalize();
+}
+
+
+std::string YouTubeDealer::getYoutubeUrl(const std::string& url)
+{
+    using namespace boost::python;
+    std::string result;
+    try
+    {
+        result = extract<std::string>(m_obj(url));
+    }
+    catch (const std::exception&)
+    {
+    }
+    catch (const error_already_set&)
+    {
+    }
+
+    return result;
+}
+
+
+} // namespace
+
+
+std::string getYoutubeUrl(const std::string& url)
+{
+    if (isUrlYoutube(url))
+    {
+        static YouTubeDealer buddy;
+        if (buddy.isValid())
+            return buddy.getYoutubeUrl(url);
+    }
+
+    return url;
+}
+
+
+#else // YOUTUBE_EXPERIMENT
+
+std::string getYoutubeUrl(const std::string& url)
+{
+    return url;
+}
+
+#endif // YOUTUBE_EXPERIMENT
---
+++ unzip/httpioapi.cpp
@@ -0,0 +1,150 @@
+/* ioapi.c -- IO base function header for compress/uncompress .zip
+   files using zlib + zip or unzip API
+
+   Version 1.01e, February 12th, 2005
+
+   Copyright (C) 1998-2005 Gilles Vollant
+
+   Modified to integrate with WinHttpRequest.
+*/
+
+#include <cassert>
+
+#include "zlib.h"
+#include "ioapi.h"
+
+#include "httprequest_h.h"
+
+#include <atlcomcli.h>
+
+
+voidpf ZCALLBACK qiodevice_open_file_func(
+    voidpf opaque,
+    voidpf filename,
+    int mode)
+{
+    VARIANT         varFalse;
+    VARIANT         varEmpty;
+
+    VariantInit(&varFalse);
+    V_VT(&varFalse) = VT_BOOL;
+    V_BOOL(&varFalse) = VARIANT_FALSE;
+
+    VariantInit(&varEmpty);
+    V_VT(&varEmpty) = VT_ERROR;
+
+    CComVariant varStream;
+
+    CComPtr<IWinHttpRequest> pIWinHttpRequest;
+
+    if (SUCCEEDED(pIWinHttpRequest.CoCreateInstance(L"WinHttp.WinHttpRequest.5.1", NULL, CLSCTX_INPROC_SERVER))
+        && SUCCEEDED(pIWinHttpRequest->Open(CComBSTR(L"GET"), CComBSTR(static_cast<const char*>(filename)), varFalse))
+        && SUCCEEDED(pIWinHttpRequest->Send(varEmpty))
+        && SUCCEEDED(pIWinHttpRequest->get_ResponseStream(&varStream)))
+    {
+        if (CComQIPtr<IStream> stream = V_UNKNOWN(&varStream))
+        {
+            return stream.Detach();
+        }
+    }
+
+    return nullptr;
+}
+
+
+uLong ZCALLBACK qiodevice_read_file_func(
+    voidpf, // opaque 
+    voidpf pf,
+    void* buf,
+    uLong size)
+{
+    IStream* stream = static_cast<IStream*>(pf);
+    uLong ret = 0;
+    HRESULT hr = stream->Read(buf, size, &ret);
+    return ret;
+}
+
+
+uLong ZCALLBACK qiodevice_write_file_func(
+    voidpf, // opaque
+    voidpf pf,
+    const void* buf,
+    uLong size)
+{
+    IStream* stream = static_cast<IStream*>(pf);
+    uLong ret = 0;
+    stream->Write(buf, size, &ret);
+    return ret;
+}
+
+uLong ZCALLBACK qiodevice_tell_file_func(
+    voidpf, // opaque
+    voidpf pf)
+{
+    IStream* stream = static_cast<IStream*>(pf);
+    const LARGE_INTEGER move{};
+    ULARGE_INTEGER libNewPosition{};
+    stream->Seek(move, STREAM_SEEK_CUR, &libNewPosition);
+    return libNewPosition.LowPart;
+}
+
+int ZCALLBACK qiodevice_seek_file_func(
+    voidpf, // opaque
+    voidpf pf,
+    uLong offset,
+    int origin)
+{
+    STREAM_SEEK dwOrigin;
+    LARGE_INTEGER move;
+    switch (origin)
+    {
+    case ZLIB_FILEFUNC_SEEK_CUR:
+        dwOrigin = STREAM_SEEK_CUR;
+        move.QuadPart = (long)offset;
+        break;
+    case ZLIB_FILEFUNC_SEEK_END:
+        dwOrigin = STREAM_SEEK_END;
+        move.QuadPart = (long)offset;
+        break;
+    case ZLIB_FILEFUNC_SEEK_SET:
+        dwOrigin = STREAM_SEEK_SET;
+        move.QuadPart = offset;
+        break;
+    default: return -1;
+    }
+    IStream* stream = static_cast<IStream*>(pf);
+    ULARGE_INTEGER libNewPosition{};
+    int ret = FAILED(stream->Seek(move, dwOrigin, &libNewPosition));
+    return ret;
+}
+
+int ZCALLBACK qiodevice_close_file_func(
+    voidpf, // opaque
+    voidpf pf)
+{
+    IStream* stream = static_cast<IStream*>(pf);
+    auto cnt = stream->Release();
+    assert(cnt == 0);
+    return 0;
+}
+
+int ZCALLBACK qiodevice_error_file_func(
+    voidpf, // opaque
+    voidpf //stream
+)
+{
+    return 0;
+}
+
+void fill_qiodevice_filefunc(
+    zlib_filefunc_def* pzlib_filefunc_def)
+{
+    pzlib_filefunc_def->zopen_file = qiodevice_open_file_func;
+    pzlib_filefunc_def->zread_file = qiodevice_read_file_func;
+    pzlib_filefunc_def->zwrite_file = qiodevice_write_file_func;
+    pzlib_filefunc_def->ztell_file = qiodevice_tell_file_func;
+    pzlib_filefunc_def->zseek_file = qiodevice_seek_file_func;
+    pzlib_filefunc_def->zclose_file = qiodevice_close_file_func;
+    pzlib_filefunc_def->zerror_file = qiodevice_error_file_func;
+    pzlib_filefunc_def->opaque = NULL;
+}
