a62035add958fed23e3ad61f1fb2666ba1d803ec
aliakseis <aliaksei.sanko@gmail.com>
2018-06-12 15:53:12 +0300

    https://www.youtube.com/watch?v=EuAzPR0ACVw

--- Player/resource.h
+++ Player/resource.h
@@ -31,13 +31,14 @@
 #define ID_TRACK3                       32773
 #define ID_TRACK4                       32774
 #define ID_AUTOPLAY                     32775
+#define ID_LOOPING                      32776
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        319
-#define _APS_NEXT_COMMAND_VALUE         32776
+#define _APS_NEXT_COMMAND_VALUE         32777
 #define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
