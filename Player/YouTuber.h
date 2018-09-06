c5436bbe6b43d6776fdabef2efe686ddc0abea68
aliakseis <aliaksei.sanko@gmail.com>
2018-09-06 18:23:17 +0300

    handling .url links - workaround for windows 10 resource caching

--- Player/YouTuber.h
+++ Player/YouTuber.h
@@ -5,4 +5,6 @@
 
 std::vector<std::string> ParsePlaylist(const std::string& url);
 
+std::vector<std::string> ParsePlaylistFile(const TCHAR* fileName);
+
 std::string getYoutubeUrl(std::string url);
