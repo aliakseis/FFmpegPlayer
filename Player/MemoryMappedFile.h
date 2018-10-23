9e6943f13701d7d8a25ae6802f969978ed3587a2
aliakseis <aliaksei.sanko@gmail.com>
2018-10-23 16:31:34 +0300

    Memory mapped file share mode fixed

--- Player/MemoryMappedFile.h
+++ Player/MemoryMappedFile.h
@@ -23,23 +23,23 @@ public:
     bool MapFlie(LPCTSTR szFile)
     {
         hFile = CreateFile(
-            szFile,					    // pointer to name of the file
-            GENERIC_READ,               // access (read-write) mode
-            0,							// share mode 
-            NULL,						// pointer to security attributes 
-            OPEN_EXISTING,				// how to create 
-            FILE_ATTRIBUTE_NORMAL,		// file attributes 
-            NULL						// handle to file with attributes to copy
+            szFile,                 // pointer to name of the file
+            GENERIC_READ,           // access (read-write) mode
+            FILE_SHARE_READ,        // share mode
+            NULL,                   // pointer to security attributes
+            OPEN_EXISTING,          // how to create
+            FILE_ATTRIBUTE_NORMAL,  // file attributes
+            NULL                    // handle to file with attributes to copy
         );
         if (INVALID_HANDLE_VALUE != hFile && GetFileSizeEx(hFile, &fileSize) && fileSize.HighPart == 0)
         {
             hFileMapping = CreateFileMapping(
-                hFile,			    // handle to file to map 
-                NULL,				// optional security attributes 
-                PAGE_READONLY,      // protection for mapping object 
-                fileSize.HighPart,	// high-order 32 bits of object size 
-                fileSize.LowPart,	// low-order 32 bits of object size 
-                NULL				// name of file-mapping object 
+                hFile,              // handle to file to map
+                NULL,               // optional security attributes
+                PAGE_READONLY,      // protection for mapping object
+                fileSize.HighPart,  // high-order 32 bits of object size
+                fileSize.LowPart,   // low-order 32 bits of object size
+                NULL                // name of file-mapping object
             );
             if (NULL != hFileMapping)
             {
