9768334d7664b74854371dd83078a8b5c816e7d8
aliakseis <aliaksei.sanko@gmail.com>
2019-06-09 21:03:44 +0300

    use http status

--- networking/http_get.cpp
+++ networking/http_get.cpp
@@ -2,6 +2,25 @@
 
 #include "httprequest_h.h"
 
+long HttpGetStatus(const char * url)
+{
+    VARIANT varFalse{ VT_BOOL };
+    VARIANT varEmpty{ VT_ERROR };
+
+    long result = 0;
+
+    CComPtr<IWinHttpRequest> pIWinHttpRequest;
+
+    if (SUCCEEDED(pIWinHttpRequest.CoCreateInstance(L"WinHttp.WinHttpRequest.5.1", NULL, CLSCTX_INPROC_SERVER))
+        && SUCCEEDED(pIWinHttpRequest->Open(CComBSTR(L"HEAD"), CComBSTR(static_cast<const char*>(url)), varFalse))
+        && SUCCEEDED(pIWinHttpRequest->Send(varEmpty)))
+    {
+        pIWinHttpRequest->get_Status(&result);
+    }
+
+    return result;
+}
+
 CComVariant HttpGet(const char * url)
 {
     VARIANT varFalse{ VT_BOOL };
--- networking/http_get.h
+++ networking/http_get.h
@@ -2,4 +2,6 @@
 
 #include <atlcomcli.h>
 
+long HttpGetStatus(const char* url);
+
 CComVariant HttpGet(const char* url);
