0ed2263462d0c37279b8b05117b953e877c689bc
aliakseis <aliaksei.sanko@gmail.com>
2021-10-12 18:55:57 +0300

    resource leak fixed

--- Player/FrameToHglobal.cpp
+++ Player/FrameToHglobal.cpp
@@ -19,9 +19,6 @@ HGLOBAL FrameToHglobal(IDirect3DSurface9* surface, int width, int height)
         return NULL;
     }
 
-    auto img_convert_ctx = sws_getContext(tmp_frame->width, tmp_frame->height, (AVPixelFormat)tmp_frame->format,
-        width, height, AV_PIX_FMT_BGR24, SWS_FAST_BILINEAR, NULL, NULL, NULL);
-
     const int stride = ((width * 3) + 3) & ~3;
 
     auto hMem = GlobalAlloc(GMEM_MOVEABLE, sizeof(BITMAPINFOHEADER) + stride * height);
@@ -49,8 +46,13 @@ HGLOBAL FrameToHglobal(IDirect3DSurface9* surface, int width, int height)
 
     const auto pData = static_cast<uint8_t*>(static_cast<void*>(bmi + 1));
 
-    sws_scale(img_convert_ctx, tmp_frame->data, tmp_frame->linesize, 0, height, 
+    auto img_convert_ctx = sws_getContext(tmp_frame->width, tmp_frame->height, (AVPixelFormat)tmp_frame->format,
+        width, height, AV_PIX_FMT_BGR24, SWS_FAST_BILINEAR, NULL, NULL, NULL);
+
+    sws_scale(img_convert_ctx, tmp_frame->data, tmp_frame->linesize, 0, height,
         &pData, &stride);
+
+    sws_freeContext(img_convert_ctx);
 
     GlobalUnlock(hMem);
     av_frame_free(&tmp_frame);
