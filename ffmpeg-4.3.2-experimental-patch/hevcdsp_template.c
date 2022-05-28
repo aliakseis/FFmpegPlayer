a6536462845501dfa685b1676069f15cef9dfbf2
aliakseis <aliaksei.sanko@gmail.com>
2022-05-28 17:53:39 +0300

    changes applied

--- ffmpeg-4.3.2-experimental-patch/hevcdsp_template.c
+++ ffmpeg-4.3.2-experimental-patch/hevcdsp_template.c
@@ -1056,7 +1056,7 @@ static void FUNC(put_hevc_qpel_bi_w_h)(uint8_t *_dst, ptrdiff_t _dststride, uint
     for (y = 0; y < height; y++) {                                                                      \
         for (x = 0; x < width; x++)                                                                     \
             dst[x] = av_clip_pixel(((QPEL_FILTER(src, 1) >> (BIT_DEPTH - 8)) * wx1 + src2[x] * wx0 +    \
-                                    ((ox0 + ox1 + 1) << log2Wd)) >> (log2Wd + 1));                      \
+                                    ((ox0 + ox1 + 1) * (1 << log2Wd))) >> (log2Wd + 1));                \
         src  += srcstride;                                                                              \
         dst  += dststride;                                                                              \
         src2 += MAX_PB_SIZE;                                                                            \
@@ -1116,7 +1116,7 @@ static void FUNC(put_hevc_qpel_bi_w_v)(uint8_t *_dst, ptrdiff_t _dststride, uint
     for (y = 0; y < height; y++) {                                                                              \
         for (x = 0; x < width; x++)                                                                             \
             dst[x] = av_clip_pixel(((QPEL_FILTER(src, srcstride) >> (BIT_DEPTH - 8)) * wx1 + src2[x] * wx0 +    \
-                                    ((ox0 + ox1 + 1) << log2Wd)) >> (log2Wd + 1));                              \
+                                    ((ox0 + ox1 + 1) * (1 << log2Wd))) >> (log2Wd + 1));                        \
         src  += srcstride;                                                                                      \
         dst  += dststride;                                                                                      \
         src2 += MAX_PB_SIZE;                                                                                    \
@@ -1209,7 +1209,7 @@ static void FUNC(put_hevc_qpel_bi_w_hv)(uint8_t *_dst, ptrdiff_t _dststride, uin
     for (y = 0; y < height; y++) {                                                                  \
         for (x = 0; x < width; x++)                                                                 \
             dst[x] = av_clip_pixel(((QPEL_FILTER(tmp, MAX_PB_SIZE) >> 6) * wx1 + src2[x] * wx0 +    \
-                                    ((ox0 + ox1 + 1) << log2Wd)) >> (log2Wd + 1));                  \
+                                    ((ox0 + ox1 + 1) * (1 << log2Wd))) >> (log2Wd + 1));            \
         tmp  += MAX_PB_SIZE;                                                                        \
         dst  += dststride;                                                                          \
         src2 += MAX_PB_SIZE;                                                                        \
@@ -1516,7 +1516,7 @@ static void FUNC(put_hevc_epel_bi_w_h)(uint8_t *_dst, ptrdiff_t _dststride, uint
     for (y = 0; y < height; y++) {
         for (x = 0; x < width; x++)
             dst[x] = av_clip_pixel(((EPEL_FILTER(src, 1) >> (BIT_DEPTH - 8)) * wx1 + src2[x] * wx0 +
-                                    ((ox0 + ox1 + 1) << log2Wd)) >> (log2Wd + 1));
+                                    ((ox0 + ox1 + 1) * (1 << log2Wd))) >> (log2Wd + 1));
         src  += srcstride;
         dst  += dststride;
         src2 += MAX_PB_SIZE;
@@ -1568,7 +1568,7 @@ static void FUNC(put_hevc_epel_bi_w_v)(uint8_t *_dst, ptrdiff_t _dststride, uint
     for (y = 0; y < height; y++) {
         for (x = 0; x < width; x++)
             dst[x] = av_clip_pixel(((EPEL_FILTER(src, srcstride) >> (BIT_DEPTH - 8)) * wx1 + src2[x] * wx0 +
-                                    ((ox0 + ox1 + 1) << log2Wd)) >> (log2Wd + 1));
+                                    ((ox0 + ox1 + 1) * (1 << log2Wd))) >> (log2Wd + 1));
         src  += srcstride;
         dst  += dststride;
         src2 += MAX_PB_SIZE;
@@ -1647,7 +1647,7 @@ static void FUNC(put_hevc_epel_bi_w_hv)(uint8_t *_dst, ptrdiff_t _dststride, uin
     for (y = 0; y < height; y++) {
         for (x = 0; x < width; x++)
             dst[x] = av_clip_pixel(((EPEL_FILTER(tmp, MAX_PB_SIZE) >> 6) * wx1 + src2[x] * wx0 +
-                                    ((ox0 + ox1 + 1) << log2Wd)) >> (log2Wd + 1));
+                                    ((ox0 + ox1 + 1) * (1 << log2Wd))) >> (log2Wd + 1));
         tmp  += MAX_PB_SIZE;
         dst  += dststride;
         src2 += MAX_PB_SIZE;
