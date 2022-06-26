5a42f007cfac4c74e22d93c44bc2f19615b30aab
aliakseis <aliaksei.sanko@gmail.com>
2022-06-26 18:32:44 +0300

    initial revision, advance, invalidate, fixed

--- Player/resource.h
+++ Player/resource.h
@@ -57,13 +57,16 @@
 #define ID_AUDIO_OPENSUBTITLES          32789
 #define ID_FIRST_SUBTITLE_DUMMY         32790
 #define ID_SUPER_RESOLUTION             32791
+#define ID_ORIENTATION_MIRRORX          32792
+#define ID_ORIENTATION_MIRRORY          32793
+#define ID_ORIENTATION_UPEND            32794
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        321
-#define _APS_NEXT_COMMAND_VALUE         32792
+#define _APS_NEXT_COMMAND_VALUE         32795
 #define _APS_NEXT_CONTROL_VALUE         1020
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
