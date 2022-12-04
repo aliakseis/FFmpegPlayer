02c4a202314bdca38ae40adcc5db8d1528dc7f12
aliakseis <aliaksei.sanko@gmail.com>
2022-12-04 15:36:39 +0300

    linux

--- QtPlayer/main.cpp
+++ QtPlayer/main.cpp
@@ -3,6 +3,7 @@
 #include <QApplication>
 
 #include <boost/log/sinks/debug_output_backend.hpp>
+#include <boost/log/sinks/text_ostream_backend.hpp>
 #include <boost/log/sinks/sync_frontend.hpp>
 #include <boost/log/core/core.hpp>
 #include <boost/log/utility/setup/common_attributes.hpp>
@@ -21,8 +22,12 @@ static void init_logging()
 
     auto core = boost::log::core::get();
 
+#ifdef Q_OS_WIN
     // Create the sink. The backend requires synchronization in the frontend.
     auto sink(boost::make_shared<boost::log::sinks::synchronous_sink<boost::log::sinks::debug_output_backend>>());
+#else
+    auto sink = boost::make_shared<boost::log::sinks::synchronous_sink<boost::log::sinks::text_ostream_backend >>();
+#endif
 
     sink->set_formatter(expr::stream
         //<< '[' << expr::format_date_time< boost::posix_time::ptime >("TimeStamp", "%H:%M:%S.%f") << ']'
